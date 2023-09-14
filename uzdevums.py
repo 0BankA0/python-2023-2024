575926103222f22c2a068a82bdeaca3456ec1575
0BankA0 <artursmurans64@gmail.com>
2023-09-14 21:53:48 +0300

    modified:   info.txt 	new file:   test.py     modified:   uzdevums.py

--- uzdevums.py
+++ uzdevums.py
@@ -12,31 +12,41 @@ class info():
         print(self.cena)
         print(self.veids)
 
-    def laboshana(self, veids, modelis,cena):
+    def laboshana(self, veids, modelis,cena,):
         self.veids = veids
         self.modelis = modelis
         self.cena = cena
+        with open('info.txt',encoding="utf=8") as file:
+            lines = file.readline()
+    
+    
+    
     def save(self):
-        with open('info.txt','w', encoding="utf=8") as fails:
+        with open('info.txt','a', encoding="utf=8") as fails:
             fails.write("-Personālā datora sastāvdaļa-\n")
             fails.write(f"Veids: {self.veids}\n")
             fails.write(f"modelis: {self.modelis}\n")
             fails.write(f"Cena: {self.cena} EUR\n")
 
-jauns = info("RAM", 'Corsair Vengeance LPX 16GB',99.99)
-jauns.apskate()
-jauns.save()
-
 
 psg.theme('darkamber')
 layout = [
             [psg.Text('Komponentes')],
-            [psg.Text('Veids'),psg.InputText()],
+            [psg.Text('Veids'),psg.InputText()],    
             [psg.Text('Modelis'),psg.InputText()],
-            [psg.Text('Cena'),psg.InputText()]
+            [psg.Text('Cena'),psg.InputText()],
+            [psg.Button('submit')]
+            
           ]
 
-layout2 = [[psg.Text('Redigēšana')]]
+layout2 = [
+    
+        [psg.Text('Redigēšana')]
+        
+
+    
+    
+    ]
 
 tabgrp = [[
     psg.TabGroup(
@@ -49,6 +59,8 @@ tabgrp = [[
         ]
     ),
     psg.Button('Aizvērt')
+    
+    
 ]]
 
 window = psg.Window('Datora komponentes', tabgrp)
@@ -58,5 +70,11 @@ while True:
     #Apgalvojumi
     if event in (psg.WIN_CLOSED,'Aizvērt'):
         break
+    elif event in (psg, 'submit'):
+        jauns = info(values[0],values[1],values[2])
+        jauns.apskate()
+        jauns.save()
+        break
+        
 
 window.close()
