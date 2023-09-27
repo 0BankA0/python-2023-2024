0dc810383a79d7d8812c2b1d159b3434a6670c88
0BankA0 <artursmurans64@gmail.com>
2023-09-28 00:29:26 +0300

    modified:   Remontdarbnica.py

--- Remontdarbnica.py
+++ Remontdarbnica.py
@@ -2,13 +2,13 @@ import PySimpleGUI as ps
 
 class Remontdarbnica():
     # Konstruktora izveide
-    def __init__(self,klienta_vards,klienta_uzvards, telefona_numurs, remontdarbu_cena, telefona_modelis, Telefonam_peiškirtais_numurs, remontdarbu_sākuma_datum, remontdarbu_beigu_datums):
+    def __init__(self,klienta_vards,klienta_uzvards, telefona_numurs, remontdarbu_cena, telefona_modelis, Telefonam_peiskirtais_numurs, remontdarbu_sākuma_datum, remontdarbu_beigu_datums):
         self.klienta_vards = klienta_vards
         self.klienta_uzvards = klienta_uzvards
         self.telefona_numurs = telefona_numurs
         self.remontdarbu_cena  =  remontdarbu_cena
         self.telefona_modelis = telefona_modelis
-        self.Telefonam_peiškirtais_numurs = Telefonam_peiškirtais_numurs
+        self.Telefonam_peiskirtais_numurs = Telefonam_peiskirtais_numurs
         self.remontdarbu_sākuma_datum = remontdarbu_sākuma_datum
         self.remontdarbu_beigu_datums = remontdarbu_beigu_datums
     # metodes
