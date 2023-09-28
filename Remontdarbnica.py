411bc85927c2f39de5a3a4eb0f7ebfca63e94f63
0BankA0 <artursmurans64@gmail.com>
2023-09-28 16:58:23 +0300

    modified:   Remontdarbnica.py 	new file:   Remontdarbnica.txt 	modified:   test.py

--- Remontdarbnica.py
+++ Remontdarbnica.py
@@ -13,7 +13,16 @@ class Remontdarbnica():
         self.remontdarbu_beigu_datums = remontdarbu_beigu_datums
     # metodes (work in progress)
     def Remontdarbu_info(self):
-        pass
+        with open('Remontdarbnica.txt','w',encoding="utf-8") as fails:
+            fails.write('=Remontdarbnica=')
+            fails.write(f'{self.klienta_vards}\n')
+            fails.write(f'{self.klienta_uzvards}\n')
+            fails.write(f'{self.telefona_numurs}\n')
+            fails.write(f'{self.remontdarbu_cena} EUR\n')
+            fails.write(f'{self.telefona_modelis}\n')
+            fails.write(f'{self.Telefonam_peiskirtais_numurs}\n')
+            fails.write(f'{self.remontdarbu_sakuma_datums}\n')
+            fails.write(f'{self.remontdarbu_beigu_datums}\n')
     def Remontdarbu_info_print(self):
         pass
 
@@ -42,7 +51,17 @@ while True:
     
     # work in progress
     if event == 'Apstiprināt':
-        pass
+        print(values)
+        klienta_vards = values[0]
+        klienta_uzvards = values[1]
+        telefona_numurs = values[2]
+        remontdarbu_cena = values[3]
+        telefona_modelis = values[4]
+        Telefonam_peiskirtais_numurs = values[5]
+        remontdarbu_sakuma_datums = values[6]
+        remontdarbu_beigu_datums = values[7]
+        save = Remontdarbnica(klienta_vards, klienta_uzvards, telefona_numurs, remontdarbu_cena, telefona_modelis, Telefonam_peiskirtais_numurs, remontdarbu_sakuma_datums, remontdarbu_beigu_datums)
+        save.Remontdarbu_info()
     if event == 'Apskatīt datus':
         pass    
 
