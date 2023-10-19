4092abccc4043865e9f04a2a36cf517c7bf02001
0BankA0 <90186471+0BankA0@users.noreply.github.com>
2023-10-19 15:30:38 +0300

    Update db_server.py

--- db_server.py
+++ db_server.py
@@ -1,5 +1,24 @@
 import mysql.connector
 
-db = mysql.connector.connect(host="localhost",database="remontdarbnica",user="root",password="123456")
-
+db = mysql.connector.connect(host="localhost",database = "123", user="root",password=" ")
 print(db)
+
+#metode .cursor()
+
+cursor = db.cursor()
+
+#.execute()
+#.execute(sql,dati)
+
+#sql kods datu ievietošanai
+sql = ("""
+insert into klients (idnew_table,klienta_vards,klienta_uzvards,telefona_numurs)
+values (%s,%s,%s,%s);
+""")
+#Ievietojamie dati
+dati = (785,"asd","zxcf",23452324)
+
+cursor.execute(sql,dati)
+
+#Apstiprināšana
+db.commit()
