94aad26e610a0ae05c01b1b223f25b70078cf22e
0BankA0 <artursmurans64@gmail.com>
2023-11-28 01:51:43 +0200

    new file:   27_11.py 	new file:   ex1data2.txt 	modified:   linear_represion_sample.py

--- 27_11.py
+++ 27_11.py
@@ -3,13 +3,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-path = os.getcwd() + '\ex1data1.txt'
-data = pd.read_csv(path, header=None, names=['Population','Profit'])
-#print(data.head())
-#print(data.describe())
+path = os.getcwd() + '\ex1data2.txt'
+data = pd.read_csv(path, header=None, names=['size','Bedrooms','Price'])
+print(data.head())
+data = (data - data.mean()) / data.std()
+print(data.head())
 
 #atelosim datus grafiski
-data.plot(kind="scatter", x="Population", y="Profit", figsize=(12,8), c= 1)
+data.plot(kind="scatter", x="Bedrooms", y="Price", figsize=(12,8), c= 1)
 plt.show()
 
 
@@ -31,7 +32,7 @@ y= data.iloc[:, cols-1:cols]
 
 X = np.matrix(X.values)
 y = np.matrix(y.values)
-theta = np.matrix(np.array([0,0]))
+theta = np.matrix(np.array([0,0,0]))
 
 print(computeCost(X,y,theta))
 
@@ -52,7 +53,7 @@ def gradientDescent(X, y, theta, alpha, iters):
        
     return theta, cost
 
-alpha = 0.01
+alpha = 5
 iters = 1000
 
 g, cost = gradientDescent(X,y,theta,alpha,iters)
@@ -61,17 +62,15 @@ print(g)
 print(computeCost(X,y,g))
 
 
-x=np.linspace(data.Population.min(),data.Population.max(), 100)
+x=np.linspace(data.Bedrooms.min(),data.Bedrooms.max(), 100)
 
 f= g[0,0] + (g[0,1]*x)
 
 fig, ax = plt.subplots(figsize=(12,8))
-ax.plot(x,f,'r', label = "Prediction")
-ax.scatter(data.Population, data.Profit, label='Traning Data')
-ax.legend(loc=2)
-ax.set_xlabel('Population')
-ax.set_ylabel('Profit')
-ax.set_title("Preditc profti vs population size")
+ax.plot(np.arange(iters), cost, 'r')
+ax.set_xlabel('Iterations')
+ax.set_ylabel('Cost')
+ax.set_title('Error vs. Training Epoch')
 
 
 plt.show()
