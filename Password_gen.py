e92e127e1a2d9eaae5e0892ab2a47d40c25de6ce
0BankA0 <90186471+0BankA0@users.noreply.github.com>
2024-01-24 14:49:41 +0200

    Update Password_gen.py

--- Password_gen.py
+++ Password_gen.py
@@ -2,14 +2,26 @@
 
 import requests #Tiek importēta bibliotēka lai varētu izmantot api
 import json #Tiek importēta json biblieotēka
+import PySimpleGUI as sg
 
-URL = "https://passwordinator.onrender.com?num=true&char=true&caps=true&len=10" #Mainīgais kura ir url uz api kurš ģenerē paroli
+def password_gen():
+    URL = "https://passwordinator.onrender.com?num=true&char=true&caps=true&len=10" #Mainīgais kura ir url uz api kurš ģenerē paroli
 
-a = requests.get(URL) #Mainīgais kurā tiek pieprasīti dati no mainīga URL
+    a = requests.get(URL) #Mainīgais kurā tiek pieprasīti dati no mainīga URL
+    dati = a.json() #Mainīgais kurā dati no mainīga a tiek parveidoti json formāta
+    print(json.dumps(dati,indent = 2)) # Tiek izvaditi dati konsole no mainigā dati
 
-dati = a.json() #Mainīgais kurā dati no mainīga a tiek parveidoti json formāta
+password_gen()
 
-print(json.dumps(dati,indent = 2)) # Tiek izvaditi dati konsole no mainigā dati
+
+sg.theme('DarkAmber')
+window = sg.Window('Password generator')
+layout = [ 
+        [sg.Text('test')],
+        
+]
+
+window.close()
 
 
      
