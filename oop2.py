4d2fe003a40d795c5ff92630b9d3d514476fc0e3
0BankA0 <90186471+0BankA0@users.noreply.github.com>
2024-05-20 15:07:44 +0300

    Add files via upload

---
+++ oop2.py
@@ -0,0 +1,21 @@
+class Kubs:
+    def __init__(self,malas_garums,krasas_nosaukums):
+        self.mals_garums = malas_garums
+        self.krasas_npsaukums = krasas_nosaukums
+
+    def aprekinat_tilpumu(self):
+        a = self.mals_garums
+        b = self.krasas_npsaukums
+
+        tilpums = a * a * a
+        print(b,tilpums)
+
+    def objekta_likvidacija():
+        pass
+
+    def kuba_ipasibas():
+        pass
+
+kubg = Kubs(10,"zaļš")
+kubg.aprekinat_tilpumu
+kubr = Kubs(1,"sarkans")
